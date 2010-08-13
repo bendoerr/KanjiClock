720a502ee3e502033cd881aabe756db56af98adc
root <root@domU-12-31-39-00-56-18.compute-1.internal>
2010-08-13 01:37:02 +0000

    Modifing the test runner

--- kanjiclock-test/src/main/java/com/fairviewfamilyfarm/kanjiclock/AllTests.java
+++ kanjiclock-test/src/main/java/com/fairviewfamilyfarm/kanjiclock/AllTests.java
@@ -3,12 +3,19 @@ package com.fairviewfamilyfarm.kanjiclock;
 import android.test.suitebuilder.TestSuiteBuilder;
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import com.synaptik.athena.AthenaTestCase
 
-public class AllTests extends TestSuite {
+public class AllTests extends AthenaTestCase {
 
     public static Test suite() {
         return new TestSuiteBuilder(AllTests.class)
                 .includeAllPackagesUnderHere()
                 .build();
+    }
+
+    public List<Class> getTestClasses() {
+	List<Class> classes = new ArrayList();
+        classes.add(KanjiNumberTests.class);
+        return classes;
     }
 }
